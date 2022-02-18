61bef01522a57095ab5e128233091eca667044a3
Mihai <plesa.mihai01@gmail.com>
2022-02-18 13:12:56 +0200

    Create main Window and main window functionalities
    Create the main window:
    Add buttons to minimize, resize and close the window.

--- HA_WinTus_Script/HA_WinTus_Script/Form1.Designer.cs
+++ HA_WinTus_Script/HA_WinTus_Script/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HA_WinTus_Script
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ClearAll = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.brnCommands = new System.Windows.Forms.Button();
+            this.btnLoops = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.bntMini = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ClearAll);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(153, 80);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(954, 477);
+            this.panel2.TabIndex = 5;
+            // 
+            // ClearAll
+            // 
+            this.ClearAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClearAll.Location = new System.Drawing.Point(867, 442);
+            this.ClearAll.Name = "ClearAll";
+            this.ClearAll.Size = new System.Drawing.Size(75, 23);
+            this.ClearAll.TabIndex = 0;
+            this.ClearAll.Text = "Clear all";
+            this.ClearAll.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.btnResize);
+            this.panel1.Controls.Add(this.bntMini);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(153, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(954, 80);
+            this.panel1.TabIndex = 4;
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(120)))), ((int)(((byte)(199)))));
+            this.panelMenu.Controls.Add(this.textBox1);
+            this.panelMenu.Controls.Add(this.brnCommands);
+            this.panelMenu.Controls.Add(this.btnLoops);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(153, 557);
+            this.panelMenu.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(120)))), ((int)(((byte)(199)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(0, 538);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 19);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "@HCE 2022";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // brnCommands
+            // 
+            this.brnCommands.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.brnCommands.Dock = System.Windows.Forms.DockStyle.Top;
+            this.brnCommands.FlatAppearance.BorderSize = 0;
+            this.brnCommands.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.brnCommands.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.brnCommands.ForeColor = System.Drawing.Color.Gainsboro;
+            this.brnCommands.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.brnCommands.Location = new System.Drawing.Point(0, 140);
+            this.brnCommands.Name = "brnCommands";
+            this.brnCommands.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.brnCommands.Size = new System.Drawing.Size(153, 60);
+            this.brnCommands.TabIndex = 2;
+            this.brnCommands.Text = "   Commands";
+            this.brnCommands.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.brnCommands.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.brnCommands.UseVisualStyleBackColor = true;
+            // 
+            // btnLoops
+            // 
+            this.btnLoops.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnLoops.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLoops.FlatAppearance.BorderSize = 0;
+            this.btnLoops.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoops.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLoops.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnLoops.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLoops.Location = new System.Drawing.Point(0, 80);
+            this.btnLoops.Name = "btnLoops";
+            this.btnLoops.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnLoops.Size = new System.Drawing.Size(153, 60);
+            this.btnLoops.TabIndex = 1;
+            this.btnLoops.Text = "   Loops";
+            this.btnLoops.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLoops.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLoops.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Image = global::HA_WinTus_Script.Properties.Resources.close_button;
+            this.btnClose.Location = new System.Drawing.Point(917, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(34, 34);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click_1);
+            // 
+            // btnResize
+            // 
+            this.btnResize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResize.FlatAppearance.BorderSize = 0;
+            this.btnResize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResize.Image = global::HA_WinTus_Script.Properties.Resources.maximize;
+            this.btnResize.Location = new System.Drawing.Point(877, 3);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(34, 34);
+            this.btnResize.TabIndex = 1;
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // bntMini
+            // 
+            this.bntMini.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bntMini.FlatAppearance.BorderSize = 0;
+            this.bntMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntMini.ForeColor = System.Drawing.SystemColors.Control;
+            this.bntMini.Image = global::HA_WinTus_Script.Properties.Resources.minimize;
+            this.bntMini.Location = new System.Drawing.Point(837, 3);
+            this.bntMini.Name = "bntMini";
+            this.bntMini.Size = new System.Drawing.Size(34, 34);
+            this.bntMini.TabIndex = 0;
+            this.bntMini.UseVisualStyleBackColor = true;
+            this.bntMini.Click += new System.EventHandler(this.bntMini_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.White;
+            this.panelLogo.BackgroundImage = global::HA_WinTus_Script.Properties.Resources.hamilton_company_logo_vector3;
+            this.panelLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(153, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1107, 557);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "MainWindow";
+            this.Text = "WinTus Script Generator";
+            this.Resize += new System.EventHandler(this.MainWindow_Resize);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button ClearAll;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Button bntMini;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button brnCommands;
+        private System.Windows.Forms.Button btnLoops;
+        private System.Windows.Forms.Panel panelLogo;
     }
 }
 
--- HA_WinTus_Script/HA_WinTus_Script/Form1.cs
+++ HA_WinTus_Script/HA_WinTus_Script/Form1.cs
@@ -7,14 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace HA_WinTus_Script
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+
+
+        public MainWindow()
         {
+            this.DoubleBuffered = true;
             InitializeComponent();
         }
+        #region <------------------- Window control ------------------->
+
+        //Drag form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        public extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NCCALCSIZE = 0x0083;
+            if (m.Msg == WM_NCCALCSIZE && m.WParam.ToInt32() == 1)
+            {
+                return;
+            }
+            base.WndProc(ref m);
+        }
+
+
+
+        private void bntMini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void MainWindow_Resize(object sender, EventArgs e)
+        {
+            AdjustForm();
+        }
+
+        private void AdjustForm()
+        {
+            switch (this.WindowState)
+            {
+                case FormWindowState.Maximized:
+                    this.Padding = new Padding(0, 8, 8, 0);
+                    break;
+                case FormWindowState.Normal:
+                    if (this.Padding.Top != 0)
+                        this.Padding = new Padding(0, 0, 0, 0);
+                    break;
+            }
+        }
+
+        private void btnClose_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #endregion
     }
 }
